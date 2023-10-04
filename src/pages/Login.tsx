1e3b99303fc25262924b9f6d1d9b7da62f6b6e19
Minh Huyen <ttminhuyen1111@gmail.com>
2023-10-04 17:10:58 +0700

    Create form login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,44 @@
-import { Box, Container } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import React from "react";
+import { Box, Container, Button, TextField } from "@mui/material";
 
 const Login = () => {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
-        <TextField id="outlined-basic" label="requied" variant="outlined" />
-        {/* <Buton onClick=()={
+  const handleLogin = () => {
+    // Handle the login logic here
+    // You can access the input value using state or refs
+  };
 
-        }></Buton> */}
+  return (
+    <Container >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Username or Email"
+          variant="outlined"
+          margin="normal"
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          variant="outlined"
+          margin="normal"
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleLogin}
+          sx={{ marginTop: 2 }}
+        >
+          Login
+        </Button>
       </Box>
     </Container>
   );
